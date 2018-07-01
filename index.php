2762cbcc51d6f9223fdd825190b616218cf52a2a
Guy Mac <hello@guymac.eu>
2018-07-01 18:34:37 +0100

    Convert to slim app

--- index.php
+++ index.php
@@ -1,50 +1,93 @@
 <?php
 
-$ignoreNames = ['errands', 'shopping'];
-$projects = getTodoistData('projects');
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
 
-$ignoreIDs = [];
-foreach ($projects as $project) {
-	if (in_array(strtolower($project['name']), $ignoreNames)) {
-		$ignoreIDs[] = $project['id'];
-	}
-}
+require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/js/keys.php';
 
-$tasks = getTodoistData('tasks');
-$days = [];
-foreach ($tasks as $i => $task) {
-	if (!$task['completed'] &&
-		!in_array($task['project_id'], $ignoreIDs) &&
-		!empty($task['due'])) {
-			$days[$task['due']['date']][] = $task;
-	}
-}
-$tasks = array_values($tasks);
+// setup Slim
 
-print_r($days);
+$config['displayErrorDetails'] = true;
+$config['addContentLengthHeader'] = false;
+$config['todoist']['key'] = $todoistApiKey;
+$config['todoist-share']['ignore'] = [
+    'shopping',
+    'errands',
+];
 
-$days = [];
-// foreach ($tasks)
+$app = new Slim\App(['settings' => $config]);
 
-function getTodoistData($uri) {
+// set up DB and templating
 
-	include __DIR__ . '/keys.php';
-	$curl = curl_init();
+$container = $app->getContainer();
 
-	curl_setopt_array($curl, [
-		CURLOPT_URL 			=> "https://beta.todoist.com/API/v8/$uri",
-  		CURLOPT_RETURNTRANSFER 	=> true,
-  		CURLOPT_HTTPHEADER 		=> ["Authorization: Bearer $todoistApiKey"],
-  	]);
+$container['view'] = function ($container) {
+    $templates = __DIR__ . '/templates/';
+    $cache = __DIR__ . '/tmp/views/';
+    $cache = false;
+    $debug = false;
+    // $debug = true;
+    $view = new Slim\Views\Twig($templates, compact('cache', 'debug'));
+    $view->getEnvironment()->addGlobal('_get', $_GET);
 
-	$result = curl_exec($curl);
-	$error = curl_error($curl);
-	curl_close($curl);
-	if ($error) {
-		return $error;
-	} else {
-		return json_decode($result, true); 
-	}
+    if ($debug) {
+        $view->addExtension(new \Slim\Views\TwigExtension(
+            $container['router'],
+            $container['request']->getUri()
+        ));
+        $view->addExtension(new \Twig_Extension_Debug());
+    }
+    return $view;
+};
+
+// routes
+
+$app->get('/', function (Request $request, Response $response) {
+
+    $projects = getTodoistData('projects', $this->get('settings')['todoist']['key']);
+    $ignoreIDs = [];
+    foreach ($projects as $project) {
+        if (in_array(strtolower($project['name']), $ignoreNames)) {
+            $ignoreIDs[] = $project['id'];
+        }
+    }
+
+    $tasks = getTodoistData('tasks', $this->get('settings')['todoist']['key']);
+    $days = [];
+    foreach ($tasks as $i => $task) {
+        if (!$task['completed'] &&
+            !in_array($task['project_id'], $ignoreIDs) &&
+            !empty($task['due'])) {
+                $days[$task['due']['date']][] = $task;
+        }
+    }
+
+});
+
+$app->run();
+
+// functions
+
+function getTodoistData($uri, $key) {
+
+    include __DIR__ . '/keys.php';
+    $curl = curl_init();
+
+    curl_setopt_array($curl, [
+        CURLOPT_URL             => "https://beta.todoist.com/API/v8/$uri",
+        CURLOPT_RETURNTRANSFER  => true,
+        CURLOPT_HTTPHEADER      => ["Authorization: Bearer $key"],
+    ]);
+
+    $result = curl_exec($curl);
+    $error = curl_error($curl);
+    curl_close($curl);
+    if ($error) {
+        return $error;
+    } else {
+        return json_decode($result, true); 
+    }
 }
 
 
